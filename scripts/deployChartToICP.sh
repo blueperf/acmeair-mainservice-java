f8e1af1754cdc351f5d9a1d9fd8a69ec8c4cd39d
Daniel Cleyrat <dacleyra@us.ibm.com>
2019-04-30 14:56:24 -0400

    chart v.1.9.0, ol

--- scripts/deployChartToICP.sh
+++ scripts/deployChartToICP.sh
@@ -16,107 +16,62 @@
 
 set -euxo pipefail
 
-CLUSTER=${1:-mycluster.icp}
+NAMESPACE="default"
+
+if [[ "${1}" == "--open-liberty" ]]
+then
+  DOCKERFILE=Dockerfile-ol-base
+  CLUSTER=${2:-mycluster.icp}
+  CHART="ibm-open-liberty"
+  SUFFIX="-ol"
+else
+  CLUSTER=${1:-mycluster.icp}
+  CHART="ibm-websphere-liberty"
+  SUFFIX=""
+fi
+
+
 
 cd "$(dirname "$0")"
 pwd
 
 helm install \
   --tls \
+  --namespace="${NAMESPACE}" \
   --name="acmeair-mainservice-java" \
-  --set-string arch.amd64="3" \
-  --set-string arch.ppc64le="0" \
-  --set-string arch.s390x="0"\
-  --set image.repository="${CLUSTER}:8500/default/acmeair-mainservice-java" \
-  --set image.pullPolicy="Always" \
-  --set service.type="ClusterIP" \
-  --set service.port="9080" \
-  --set service.targetPort="9080" \
-  --set service.name="acmeair-main-service" \
-  --set ssl.enabled="false" \
-  --set ingress.enabled="true" \
-  --set ingress.path="/acmeair" \
-  --set ingress.rewriteTarget="/acmeair" \
-  ../chart/ibm-websphere-liberty 
+  --set ${CHART}.image.repository="${CLUSTER}:8500/${NAMESPACE}/acmeair-mainservice-java" \
+  ../chart${SUFFIX}/acmeair-mainservice-java
 
   
 helm install \
   --tls \
+  --namespace="${NAMESPACE}" \
   --name="acmeair-authservice-java" \
-  --set-string arch.amd64="3" \
-  --set-string arch.ppc64le="0" \
-  --set-string arch.s390x="0"\
-  --set image.repository="${CLUSTER}:8500/default/acmeair-authservice-java" \
-  --set image.pullPolicy="Always" \
-  --set service.type="ClusterIP" \
-  --set service.port="9080" \
-  --set service.targetPort="9080" \
-  --set service.name="acmeair-auth-service" \
-  --set ssl.enabled="false" \
-  --set ingress.enabled="true" \
-  --set ingress.path="/auth" \
-  --set ingress.rewriteTarget="/" \
-  --set env.jvmArgs="-Dcom.acmeair.client.CustomerClient/mp-rest/url=http://acmeair-customer-service:9080" \
-  ../../acmeair-authservice-java/chart/ibm-websphere-liberty
+  --set ${CHART}.image.repository="${CLUSTER}:8500/${NAMESPACE}/acmeair-authservice-java" \
+  ../../acmeair-authservice-java/chart${SUFFIX}/acmeair-authservice-java
   
   
   
 helm install \
   --tls \
+  --namespace="${NAMESPACE}" \
   --name="acmeair-bookingservice-java" \
-  --set-string arch.amd64="3" \
-  --set-string arch.ppc64le="0" \
-  --set-string arch.s390x="0"\
-  --set image.repository="${CLUSTER}:8500/default/acmeair-bookingservice-java" \
-  --set image.pullPolicy="Always" \
-  --set service.type="ClusterIP" \
-  --set ssl.enabled="false" \
-  --set service.port="9080" \
-  --set service.targetPort="9080" \
-  --set service.name="acmeair-booking-service" \
-  --set ingress.enabled="true" \
-  --set ingress.path="/booking" \
-  --set ingress.rewriteTarget="/" \
-  --set env.jvmArgs="-Dcom.acmeair.client.CustomerClient/mp-rest/url=http://acmeair-customer-service:9080 -Dcom.acmeair.client.FlightClient/mp-rest/url=http://acmeair-flight-service:9080 -DMONGO_HOST=acmeair-booking-db" \
-  ../../acmeair-bookingservice-java/chart/ibm-websphere-liberty
+  --set ${CHART}.image.repository="${CLUSTER}:8500/${NAMESPACE}/acmeair-bookingservice-java" \
+  ../../acmeair-bookingservice-java/chart${SUFFIX}/acmeair-bookingservice-java
 
   
 helm install \
   --tls \
+  --namespace="${NAMESPACE}" \
   --name="acmeair-customerservice-java" \
-  --set-string arch.amd64="3" \
-  --set-string arch.ppc64le="0" \
-  --set-string arch.s390x="0"\
-  --set image.repository="${CLUSTER}:8500/default/acmeair-customerservice-java" \
-  --set image.pullPolicy="Always" \
-  --set service.type="ClusterIP" \
-  --set ssl.enabled="false" \
-  --set service.port="9080" \
-  --set service.targetPort="9080" \
-  --set service.name="acmeair-customer-service" \
-  --set ingress.enabled="true" \
-  --set ingress.path="/customer" \
-  --set ingress.rewriteTarget="/" \
-  --set env.jvmArgs="-DMONGO_HOST=acmeair-customer-db" \
-  ../../acmeair-customerservice-java/chart/ibm-websphere-liberty
+  --set ${CHART}.image.repository="${CLUSTER}:8500/${NAMESPACE}/acmeair-customerservice-java" \
+  ../../acmeair-customerservice-java/chart${SUFFIX}/acmeair-customerservice-java
   
   
 helm install \
   --tls \
+  --namespace="${NAMESPACE}" \
   --name="acmeair-flightservice-java" \
-  --set-string arch.amd64="3" \
-  --set-string arch.ppc64le="0" \
-  --set-string arch.s390x="0"\
-  --set image.repository="${CLUSTER}:8500/default/acmeair-flightservice-java" \
-  --set image.pullPolicy="Always" \
-  --set service.type="ClusterIP" \
-  --set ssl.enabled="false" \
-  --set service.port="9080" \
-  --set service.targetPort="9080" \
-  --set service.name="acmeair-flight-service" \
-  --set ingress.enabled="true" \
-  --set ingress.path="/flight" \
-  --set ingress.rewriteTarget="/" \
-  --set env.jvmArgs="-DMONGO_HOST=acmeair-flight-db" \
-  ../../acmeair-flightservice-java/chart/ibm-websphere-liberty 
+  --set ${CHART}.image.repository="${CLUSTER}:8500/${NAMESPACE}/acmeair-flightservice-java" \
+  ../../acmeair-flightservice-java/chart${SUFFIX}/acmeair-flightservice-java
 
