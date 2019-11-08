be3687a295c868944e7ca4e5f6c751e0574a3ca1
Joe McClure <jdmcclur@us.ibm.com>
2019-11-08 15:43:19 -0600

    Take out MC code

--- terraform/scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-mvn clean install
-target/liberty/wlp/bin/server package defaultServer --archive="acmeairmainservicejava" --include=minify
--- terraform/scripts/start.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-pkill java
-unzip acmeairmainservicejava.zip
-wlp/bin/server start
