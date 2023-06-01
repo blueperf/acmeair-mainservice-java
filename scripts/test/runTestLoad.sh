0f9e4ae50e0f332de974e398f9fdac777b97da06
ruizhao <rui.zhao1@ibm.com>
2023-06-01 17:05:52 -0500

    add quick test

---
+++ scripts/test/runTestLoad.sh
@@ -0,0 +1,1 @@
+${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t ../../jmeter-files/AcmeAir-microservices-mpJwt-header.jmx -JHOST=192.168.90.166 -JPORT=80 -JTHREAD=10 -JUSER=999 -JDURATION=30 -JRAMP=0
