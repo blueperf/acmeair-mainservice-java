12e789ad69f4f0708a7193352494a810ea8408d4
ruizhao <rui.zhao1@ibm.com>
2023-08-15 14:42:25 -0500

    update wildfly

--- scripts/test/testAll.sh
+++ scripts/test/testAll.sh
@@ -4,7 +4,6 @@ cd $(dirname "$0")
 CWD=$(pwd)
 
 cd ../../
-
 docker-compose -f docker-compose-v3.yml down > /dev/null 2>&1
 sleep 4
 docker-compose -f docker-compose-v3.yml up --build -d > /dev/null 2>&1
@@ -16,6 +15,16 @@ cd ${CWD}
 
 cd ../../
 docker-compose -f docker-compose-v3.yml down > /dev/null 2>&1
+sleep 4
+docker-compose -f docker-compose-wf-v3.yml up --build -d > /dev/null 2>&1
+sleep 30
+
+cd ${CWD}
+./loadDB.sh
+./runTestLoad.sh
+
+cd ../../
+docker-compose -f docker-compose-wf-v3.yml down > /dev/null 2>&1
 sleep 4
 docker-compose -f docker-compose-pm-v3.yml up --build -d > /dev/null 2>&1
 sleep 30
